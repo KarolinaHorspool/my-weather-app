ad92ce254ad346cae97517035fcdc17db5a423a7
KarolinaHorspool <karolina.horspool@gmail.com>
2021-01-24 07:38:43 +0000

    footer amended

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from "react";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather Search </h1>
-      <footer>
-        This project is <p className="github-link"> open-sourced on GitHub <a href="https://github.com/KarolinaHorspool/my-weather-app"
-        target="_blank"> is hosted on Netlify and was coded </a>, by Karolina Horspool</p>
-      </footer>
-    </div>
+        <h1>Weather Search </h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/hall-of-fame?q=Karolina+Horspool"
+            target="_blank"
+          >
+            Karolina Horspool
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/KarolinaHorspool/my-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
