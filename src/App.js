34cedf7c6866850d2fc5807ea5491ecc22deb519
KarolinaHorspool <karolina.horspool@gmail.com>
2021-01-24 07:28:01 +0000

    Updating footer

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-
-      
       <h1>Weather Search </h1>
+      <footer>
+        This project is <p className="github-link"> open-sourced on GitHub <a href="https://github.com/KarolinaHorspool/my-weather-app"
+        target="_blank"> <p className="netlify-link"></p>is hosted on Netlify and was coded </a>, by Karolina Horspool</p>
+      </footer>
     </div>
     </div>
   );
