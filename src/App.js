8ee63acf3f7800f82e5b07d1726737099b86af56
KarolinaHorspool <karolina.horspool@gmail.com>
2021-01-24 06:47:57 +0000

    Imported bootstrap and added container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
+
+      
       <h1>Weather Search </h1>
+    </div>
     </div>
   );
 }
