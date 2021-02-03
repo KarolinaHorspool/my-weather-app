3992c4a6dad6c2bd62a644d602f104884590bd1b
KarolinaHorspool <karolina.horspool@gmail.com>
2021-02-03 20:54:07 +0000

    Clearing up styling

--- src/App.js
+++ src/App.js
@@ -5,35 +5,37 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Weather defaultCity="London" />
-        <footer>
-          This project was coded by{" "}
-          <a
-            href="https://www.shecodes.io/students/244-karolina-horspool"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Karolina Horspool
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/KarolinaHorspool/my-weather-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://brave-engelbart-d9c046.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hosted on Netlify
-          </a>
-        </footer>
+      <div className="box">
+        <div className="container">
+          <Weather defaultCity="London" />
+        </div>
       </div>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.shecodes.io/students/244-karolina-horspool"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Karolina Horspool
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/KarolinaHorspool/my-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://brave-engelbart-d9c046.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
