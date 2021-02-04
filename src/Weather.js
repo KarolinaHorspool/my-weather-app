3d4cfe6b95beeb11fa2951628701f4cfb03266eb
KarolinaHorspool <karolina.horspool@gmail.com>
2021-02-04 20:49:41 +0000

    Fixed temperature component in Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -12,6 +12,7 @@ export default function Weather(props) {
     setWeatherData({
       ready: true,
       humidity: response.data.main.humidity,
+      temperature: response.data.main.temp,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       icon: response.data.weather[0].icon,
