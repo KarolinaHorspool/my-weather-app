93cacf19338f9906a2cbebb3e11a96f01ec70c0f
KarolinaHorspool <karolina.horspool@gmail.com>
2021-02-06 09:24:24 +0000

    Formatted footer styling

--- src/App.js
+++ src/App.js
@@ -12,31 +12,33 @@ export default function App() {
           </div>
         </div>
       </div>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/students/244-karolina-horspool"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Karolina Horspool
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/KarolinaHorspool/my-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://brave-engelbart-d9c046.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hosted on Netlify
-        </a>
+      <footer className="Footer">
+        <p>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/students/244-karolina-horspool"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Karolina Horspool
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/KarolinaHorspool/my-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://brave-engelbart-d9c046.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </p>
       </footer>
     </div>
   );
